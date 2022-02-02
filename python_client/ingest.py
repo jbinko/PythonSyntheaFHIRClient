22eb3e3bc7fbbda00fb8718ca0ac8f3662ab7afb
Ubuntu <vmadmin@synthea-host.ip4dmbsa5isudj4aoeu3pt14zb.cx.internal.cloudapp.net>
2022-02-02 17:49:37 +0000

    Added check for existing upload/error directories

--- python_client/ingest.py
+++ python_client/ingest.py
@@ -26,21 +26,27 @@ def process_uploaded(upload_result,target_path):
 	uploaded_files = upload_result["uploaded"]
 	error_files = upload_result["error"]
 	output_upload_path = target_path+"/uploaded"
+        uploaded_exists = os.path.isfile(output_upload_path) 
 	output_error_path = target_path+"/error"
+        error_exists = os.path.isfile(output_error_path)
+        if not uploaded_exists:
+            os.mkdir(output_upload_path)
+        if not error_exists:
+            os.mkdir(output_error_path)
 	for uploaded_file in uploaded_files:
 		shutil.move(uploaded_file,output_upload_path)
 	for error_file in error_files:
 		shutil.move(error_file,output_error_path)
 	
 def main():
-	connection_string = "DefaultEndpointsProtocol=https;AccountName=fhirlanding;AccountKey=CmrotBz6cCiWCff468oGsloGEPH8bRlkdpEevz+LjtSSKzYrntSZyRbhicrXUQa9uYgpwxkz3zdRCl7+RrCE9A==;EndpointSuffix=core.windows.net"
+	connection_string = "DefaultEndpointsProtocol=https;AccountName=fhirimporterappsa;AccountKey=Jk9HyUdWiGpTm9jk6VSadWN6xfPQTZrLCFXpbng4mzSGfAXQX8LEMa9yODgyA8QGhlgLOSGrOalZqqxsxCLvcg==;EndpointSuffix=core.windows.net"
 	# Create the BlobServiceClient object which will be used to create a container client
 	blob_service_client = BlobServiceClient.from_connection_string(connection_string)
         # Create a unique name for the container
-	container_name = "fhirsynthea"
+	container_name = "fhirimport"
 	local_output_path = dir_path = os.path.dirname(os.path.realpath(__file__))
 	print(local_output_path)
-	local_file_path = "/home/vmadmin/synthea/output/fhir/fhir"
+	local_file_path = "/home/vmadmin/synthea/output/fhir"
 	#while(True):
 	print("Checking path: "+local_file_path+" for new files...")
 	upload_result = list_and_upload_new(blob_service_client,container_name,local_file_path)
