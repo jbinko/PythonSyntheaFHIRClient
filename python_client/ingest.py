24577abb777be7b1caba9ce5f86ca9fd8e7c1600
Ubuntu <vmadmin@synthea-host.ip4dmbsa5isudj4aoeu3pt14zb.cx.internal.cloudapp.net>
2022-02-03 17:23:41 +0000

    Updated variable names, restructuring

--- python_client/ingest.py
+++ python_client/ingest.py
@@ -8,7 +8,7 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, _
 
 class Config:
 	def __init__(self,filename):
-		self.required_config_keys = ["polling_interval","output_path","connection_string","container_name"]
+		self.required_config_keys = ["polling_interval","FHIR_output_path","connection_string","container_name"]
 		config_file = open(filename,"r")
 		self.config_map = json.load(config_file)
 		config_file.close()
@@ -22,42 +22,60 @@ class Config:
 		return configOK
 	def get_config_map(self):
 		return self.config_map
-def list_and_upload_new(blob_service_client,container_name, local_path):
-	files = os.listdir(local_path)
-	results = {}
-	results["uploaded"] = []
-	results["error"] = []
-	# Create the container
-	for current_file in files:
-		if ".json" in current_file:
-			file_with_path = local_path+"/"+current_file
-			try:
-				file_to_upload = open(file_with_path).read()
-				blob_client = blob_service_client.get_blob_client(container=container_name,blob=current_file)
-				print("Uploading "+current_file+" to blob account...")
-				blob_client.upload_blob(file_to_upload)
-				results["uploaded"].append(file_with_path)
-			except Exception as ex:
-				print('Exception:')
-				print(ex)
-				results["error"].append(file_with_path)
-	return results
-def process_uploaded(upload_result,target_path):
-	uploaded_files = upload_result["uploaded"]
-	error_files = upload_result["error"]
-	output_upload_path = target_path+"/uploaded"
-	uploaded_exists = os.path.exists(output_upload_path) 
-	output_error_path = target_path+"/error"
-	error_exists = os.path.exists(output_error_path)
-	if not uploaded_exists:
-		os.mkdir(output_upload_path)
-	if not error_exists:
-		os.mkdir(output_error_path)
-	for uploaded_file in uploaded_files:
-		shutil.move(uploaded_file,output_upload_path)
-	for error_file in error_files:
-		shutil.move(error_file,output_error_path)
-	
+class FHIRUploader:
+	def __init__(self,connection_string,container_name,polling_interval,FHIR_output_path, local_output_path):
+		self.connection_string = connection_string
+		self.container_name = container_name
+		self.polling_interval = polling_interval
+		self.FHIR_output_path = FHIR_output_path
+		self.local_output_path = local_output_path
+	def init_blob_connection(self): 
+		# Create the BlobServiceClient object which will be used to create a container client
+		blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
+		return blob_service_client
+	def set_blob_connection(self,blob_service_client):
+		self.blob_service_client = blob_service_client
+	def list_and_upload_new(self):
+		files = os.listdir(self.FHIR_output_path)
+		results = {}
+		results["uploaded"] = []
+		results["error"] = []
+		# Create the container
+		for current_file in files:
+			if ".json" in current_file:
+				file_with_path = self.FHIR_output_path+"/"+current_file
+				try:
+					file_to_upload = open(file_with_path,'r').read()
+					blob_client = self.blob_service_client.get_blob_client(container=self.container_name,blob=current_file)
+					print("Uploading "+current_file+" to blob account...")
+					blob_client.upload_blob(file_to_upload)
+					results["uploaded"].append(file_with_path)
+				except Exception as ex:
+					print('Exception:')
+					print(ex)
+					results["error"].append(file_with_path)
+		return results
+	def run_upload_process(self):
+		while(True):
+			print("Checking path: "+self.FHIR_output_path+" for new files...")
+			upload_result = self.list_and_upload_new()
+			self.process_uploaded(upload_result,self.local_output_path)
+			time.sleep(self.polling_interval)
+	def process_uploaded(self,upload_result,target_path):
+		uploaded_files = upload_result["uploaded"]
+		error_files = upload_result["error"]
+		output_upload_path = target_path+"/uploaded"
+		uploaded_exists = os.path.exists(output_upload_path) 
+		output_error_path = target_path+"/error"
+		error_exists = os.path.exists(output_error_path)
+		if not uploaded_exists:
+			os.mkdir(output_upload_path)
+		if not error_exists:
+			os.mkdir(output_error_path)
+		for uploaded_file in uploaded_files:
+			shutil.move(uploaded_file,output_upload_path)
+		for error_file in error_files:
+			shutil.move(error_file,output_error_path)
 def main():
 	if len(sys.argv) < 2:
 		print("No configuration file given.")
@@ -71,16 +89,14 @@ def main():
 	connection_string = config_map["connection_string"] 
 	container_name = config_map["container_name"]
 	polling_interval = int(config_map["polling_interval"])
-	local_file_path = config_map["output_path"]
+	FHIR_output_path = config_map["FHIR_output_path"]
+	local_output_path = config_map["local_output_path"]
 	print("Config file loaded successfully.\n Polling interval: "+str(polling_interval)+"s\n")
-	# Create the BlobServiceClient object which will be used to create a container client
-	blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-	# Create a unique name for the container
-	local_output_path = dir_path = os.path.dirname(os.path.realpath(__file__))
-	while(True):
-		print("Checking path: "+local_file_path+" for new files...")
-		upload_result = list_and_upload_new(blob_service_client,container_name,local_file_path)
-		process_uploaded(upload_result,local_output_path)
-		time.sleep(polling_interval)
+	if local_output_path == None or local_output_path == "":
+		local_output_path = os.path.dirname(os.path.realpath(__file__))
+	uploader = FHIRUploader(connection_string, container_name, polling_interval, FHIR_output_path,local_output_path)
+	blob_service_client = uploader.init_blob_connection()
+	uploader.set_blob_connection(blob_service_client)
+	uploader.run_upload_process()
 if __name__ == "__main__":
 	main()
