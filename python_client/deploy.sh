b1e6580b690c38ead6b1a0ade53ca6f96e252780
Ubuntu <vmadmin@synthea-host.ip4dmbsa5isudj4aoeu3pt14zb.cx.internal.cloudapp.net>
2022-03-03 18:53:40 +0000

    Added deployment script to create config file

---
+++ python_client/deploy.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+connection_string=$1		
+polling_interval=$2		
+FHIR_output_path=$3		
+local_output_path=$4		
+log_path=$5		
+container_name=$6		
+
+echo "Writing config file..."
+
+content=$'{\n\"connection_string\":\"'"${connection_string}"$'\",\n\"polling_interval\":\"'"${polling_interval}"$'\",\n\"FHIR_output_path\":\"'"${FHIR_output_path}"$'\",\n\"local_output_path\":\"'"${local_output_path}"$'\",\n\"log_path\":\"'"${log_path}"$'\",\n\"container_name\":\"'"${container_name}"$'\"\n}' 
+
+echo "$content" > deploy_config.json
