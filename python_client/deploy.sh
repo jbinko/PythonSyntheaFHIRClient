d7b4c9787012be2fbd7b5c550cd69b9054ab6b3f
jbinko <jbinko+github@gmail.com>
2022-03-06 14:42:15 +0100

    Update deploy.sh
    Added git clone

--- python_client/deploy.sh
+++ python_client/deploy.sh
@@ -6,6 +6,9 @@ local_output_path=$4
 log_path=$5		
 container_name=$6		
 
+git clone https://github.com/djdean/PythonSyntheaFHIRClient
+cd PythonSyntheaFHIRClient
+
 echo "Writing config file..."
 
 content=$'{\n\"connection_string\":\"'"${connection_string}"$'\",\n\"polling_interval\":\"'"${polling_interval}"$'\",\n\"FHIR_output_path\":\"'"${FHIR_output_path}"$'\",\n\"local_output_path\":\"'"${local_output_path}"$'\",\n\"log_path\":\"'"${log_path}"$'\",\n\"container_name\":\"'"${container_name}"$'\"\n}' 
